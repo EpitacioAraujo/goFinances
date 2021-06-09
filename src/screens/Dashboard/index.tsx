bb4701c185073404148f1ab9b6cfdf4a4b3abb0f
DESKTOP-V4N1IV2\epita <epitacio.sobrinho00@hotmail.com>
2021-06-09 12:32:00 -0300

    created HighLightCard component and show this in dashborad screen

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,55 @@
 import React from 'react';
-import { Container, Title } from './styles';
+
+import { 
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighLightCards
+} from './styles';
+import { HighLightCard } from '../../components/HighLightCard';
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>
-                Dashborad
-            </Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{ uri: 'https://github.com/EpitacioAraujo.png' }} />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Epitacio</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
+
+            <HighLightCards>
+                <HighLightCard 
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 17.400,00"
+                    lastTansaction="Última entrada dia 13 de abril"
+                />
+                <HighLightCard 
+                    type="down"
+                    title="Saidas"
+                    amount="R$ 1.259,00"
+                    lastTansaction="Última entrada dia 03 de abril"
+                />
+                <HighLightCard 
+                    type="total"
+                    title="Total"
+                    amount="R$ 16.141,00"
+                    lastTansaction="01 à 06 de abril"
+                />
+            </HighLightCards>
         </Container>
     )
 }
