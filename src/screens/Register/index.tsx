8d078bf36b5d0ea3dd0a099ad8814d1b61fe84c7
DESKTOP-V4N1IV2\epita <epitacio.sobrinho00@hotmail.com>
2021-06-24 21:28:54 -0300

    created button component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Input } from '../../components/Forms/Input';
+import { Button } from '../../components/Forms/Button/Index';
 
 import {
     Container,
     Header,
     Title,
-    Form
+    Form,
+    Fields
 } from './styles'
 
 export function Register() {
@@ -18,8 +20,12 @@ export function Register() {
             </Header>
 
             <Form>
-                <Input placeholder="Nome" />
-                <Input placeholder="Preço" />
+                <Fields>
+                    <Input placeholder="Nome" />
+                    <Input placeholder="Preço" />
+                </Fields>
+
+                <Button title="Enviar" />
             </Form>
         </Container>
     );
